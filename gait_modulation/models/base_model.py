2e9cd4720f149158a3ea6ade9a01f0101df59a95
Mohammad <orabe.mhd@gmail.com>
2024-11-20 15:53:53 +0100

    feat: add LFP metadata configuration and update data preprocessing with rejection criteria refactor: remove logistic regression model implementation and update base model evaluation method

--- gait_modulation/models/base_model.py
+++ gait_modulation/models/base_model.py
@@ -25,9 +25,9 @@ class BaseModel(ABC):
             'confusion_matrices': [] if metric_config.get("confusion_matrix", False) else None
         }
 
-    @abstractmethod
-    def build_model(self):
-        pass
+    # @abstractmethod
+    # def build_model(self):
+    #     pass
 
     @abstractmethod
     def fit(self, X_train, y_train):
@@ -36,27 +36,25 @@ class BaseModel(ABC):
     @abstractmethod
     def predict(self, X_test):
         pass
-        
-    def evaluate(self, splits, n_bands=None):
-        for X_train, X_test, y_train, y_test in splits:
-            if n_bands:
-                X_train = self.reshape_for_lstm(X_train, n_bands)
-                X_test = self.reshape_for_lstm(X_test, n_bands)
+   
+    def evaluate(self, y_true, y_pred):
+        """
+        Evaluate metrics based on true labels and predictions.
 
-            self.fit(X_train, y_train)
-            y_pred = self.predict(X_test)
-
-            # Evaluate metrics
-            if self.metrics.get('accuracy') is not None:
-                self.metrics['accuracy'].append(accuracy_score(y_test, y_pred))
-            if self.metrics.get('precision') is not None:
-                self.metrics['precision'].append(precision_score(y_test, y_pred, average='weighted'))
-            if self.metrics.get('recall') is not None:
-                self.metrics['recall'].append(recall_score(y_test, y_pred, average='weighted'))
-            if self.metrics.get('f1') is not None:
-                self.metrics['f1'].append(f1_score(y_test, y_pred, average='weighted'))
-            if self.metrics.get('confusion_matrices') is not None:
-                self.metrics['confusion_matrices'].append(confusion_matrix(y_test, y_pred))
+        Parameters:
+        - y_true: Ground truth labels
+        - y_pred: Predicted labels or values
+        """
+        if self.metrics.get('accuracy') is not None:
+            self.metrics['accuracy'].append(accuracy_score(y_true, y_pred))
+        if self.metrics.get('precision') is not None:
+            self.metrics['precision'].append(precision_score(y_true, y_pred, average='weighted'))
+        if self.metrics.get('recall') is not None:
+            self.metrics['recall'].append(recall_score(y_true, y_pred, average='weighted'))
+        if self.metrics.get('f1') is not None:
+            self.metrics['f1'].append(f1_score(y_true, y_pred, average='weighted'))
+        if self.metrics.get('confusion_matrices') is not None:
+            self.metrics['confusion_matrices'].append(confusion_matrix(y_true, y_pred))
 
         # Print metrics summary
         for key, values in self.metrics.items():
@@ -65,3 +63,32 @@ class BaseModel(ABC):
             elif values is not None:
                 for i, cm in enumerate(values):
                     print(f"Confusion Matrix for fold {i + 1}:\n{cm}\n")
+    # OLD CODE
+    # def evaluate(self, splits, n_bands=None):
+    #     for X_train, X_test, y_train, y_test in splits:
+    #         if n_bands:
+    #             X_train = self.reshape_for_lstm(X_train, n_bands)
+    #             X_test = self.reshape_for_lstm(X_test, n_bands)
+
+    #         self.fit(X_train, y_train)
+    #         y_pred = self.predict(X_test)
+
+    #         # Evaluate metrics
+    #         if self.metrics.get('accuracy') is not None:
+    #             self.metrics['accuracy'].append(accuracy_score(y_test, y_pred))
+    #         if self.metrics.get('precision') is not None:
+    #             self.metrics['precision'].append(precision_score(y_test, y_pred, average='weighted'))
+    #         if self.metrics.get('recall') is not None:
+    #             self.metrics['recall'].append(recall_score(y_test, y_pred, average='weighted'))
+    #         if self.metrics.get('f1') is not None:
+    #             self.metrics['f1'].append(f1_score(y_test, y_pred, average='weighted'))
+    #         if self.metrics.get('confusion_matrices') is not None:
+    #             self.metrics['confusion_matrices'].append(confusion_matrix(y_test, y_pred))
+
+    #     # Print metrics summary
+    #     for key, values in self.metrics.items():
+    #         if values is not None and key != 'confusion_matrices':
+    #             print(f"{key.capitalize()} - Mean: {np.mean(values):.2f}, Std: {np.std(values):.2f}")
+    #         elif values is not None:
+    #             for i, cm in enumerate(values):
+    #                 print(f"Confusion Matrix for fold {i + 1}:\n{cm}\n")
